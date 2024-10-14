973c6ef50df01bd62604303d85be920bf2d1ef15
Pascal <psibondagara@outlook.com>
2024-10-14 15:24:17 +0200

    some light changes

--- src/app/pascal/page.js
+++ src/app/pascal/page.js
@@ -4,7 +4,7 @@ export default function pascal(){
     return(
         <>
         <div>
-            Nigga. dette er pascal siden
+            dette er pascal siden
         </div>
         </>
     )
